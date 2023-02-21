9dae7bceea45d11df0e0c5351e9724f5f00933eb
Graham Sprague <graham.sprague@gmail.com>
2023-02-21 16:14:19 -0500

    added stairs and playable level

--- main.js
+++ main.js
@@ -49,118 +49,113 @@ function load_room(map){
 
 // navigation events up, down, left, right keys
 $(document).on('keydown',function(e) {
-    //get current position
+    // get current position
     var position = $('.player').position();
     var left_int = parseInt(position.left);
     var top_int  = parseInt(position.top);
+    // define map bounds
     var low_bound = -25;
     var bounding_top = 425;
     var bounding_left = 830;
+    // movement and collision
     var move_units = 10;
     var move_duration = 0;
     var col = false;
 
-    // calc player center point
+    // calc player center point(pp)
     var pl= $('.player')[0].getBoundingClientRect();
-
     var pp = {};
     pp['t'] = pl.bottom - (pl.height/2);
     pp['l'] = pl.right - (pl.width/2);
 
+    // create a copy to store potential movement point
     var pp_new = jQuery.extend(true, {}, pp);
 
+    // handle key down events
     switch (e.keyCode) {
         case 37:
-            // LEFT
-
+            console.log('left');
             var new_left = left_int - move_units;
             
-            // check boundry
+            // check boundry to prevent walking out side of play area
             if( new_left <= low_bound ){
                 console.log('bounds reached');
                 break;
             }
-            console.log('left');
-
+            
             // create proposed bounds
             pp_new.l = pp_new.l - move_units;
             
             // check for collision
-            col = check_collision_new( pp_new , $('.block, .door, .item') );
+            col = check_collision_new( pp_new , $('.block, .door, .item, .stairs') );
 
+            // if no collision, allow movement to new position
             if (col == false){
                 var next_image = get_next_image( 'left' );
                 $('.player').animate({left: new_left + 'px'}, move_duration);
             }
             break;
         case 39:
-
-            // RIGHT
-
+            console.log('right');
             var new_left = left_int + move_units;
-            // check boundry
+
+            // check boundry to prevent walking out side of play area
             if( new_left >= bounding_left ){
                 console.log('bounds reached');
                 break;
             }
-            
-            console.log('right');
 
             // create proposed bounds
             pp_new.l = pp_new.l + move_units;
 
             // check for collision
-            col = check_collision_new( pp_new , $('.block, .door, .item') );
+            col = check_collision_new( pp_new , $('.block, .door, .item, .stairs') );
             
+            // if no collision, allow movement to new position
             if (col == false){
                 var next_image = get_next_image( 'right' );
                 $('.player').animate({left: new_left + 'px'}, move_duration);
             }
-            
             break;
         case 38:
-
-            // UP
-
+            console.log('up');
             var new_top = top_int - move_units;
+
             // check boundry
             if( new_top <= low_bound ){
                 console.log('bounds reached');
                 break;
             }
-            console.log('up');
             
             // create proposed bounds
             pp_new.t = pp_new.t - move_units;
 
             // check for collision
-            col = check_collision_new( pp_new , $('.block, .door, .item') );
+            col = check_collision_new( pp_new , $('.block, .door, .item, .stairs') );
 
+            // if no collision, allow movement to new position
             if (col === false){
                 var next_image = get_next_image( 'up' );
                 $('.player').animate({top: new_top + 'px'}, move_duration);
             }
-            
             break;
         case 40:
-
-            // DOWN
-
+            console.log('down');
             var new_top = top_int + move_units;
+
             // check boundry
             if( new_top >= bounding_top ){
                 console.log('bounds reached');
                 break;
             }
-            console.log('down');
-
+           
             // create proposed bounds
-            
             pp_new.t = pp_new.t + move_units;
 
             // check for collision
-            col = check_collision_new( pp_new , $('.block, .door, .item') );
-            console.log('col:'+ col);
+            col = check_collision_new( pp_new , $('.block, .door, .item, .stairs') );
+
+            // if no collision, allow movement to new position
             if (col == false){
                 var next_image = get_next_image( 'down' );
                 $('.player').animate({top: new_top + 'px'}, move_duration);
@@ -174,9 +169,11 @@ $(document).on('keydown',function(e) {
 
 function check_collision_new(my_pp,my_blocks){
 
+    //buffer around player center
     var col_pad = 5;
     my_col = false;
 
+    // loop over all potential collision objects
     my_blocks.each(function(index) {
 
         // isolate the current block
@@ -186,18 +183,24 @@ function check_collision_new(my_pp,my_blocks){
 
         // get bounds of the current block
         var bb = my_block.getBoundingClientRect();
- 
+        
+        // are we in collision?
         if ( 
             my_pp.t + col_pad >= bb.top &&
             my_pp.t - col_pad <= bb.bottom &&
             my_pp.l + col_pad >= bb.left &&
             my_pp.l - col_pad <= bb.right
         ){
+            // yes we are colliding
             console.log('collision!!');
             console.log(my_block.id);
+            // capture the block we are in collision with
             var col_id = my_block.id;
             my_col = true;
+
+            // what are we in collision if its not just a block?
             if($(my_block).hasClass('door')){
+                //DOOR
                 console.log('DOOR:'+ col_id);
 
                 if( 
@@ -212,10 +215,10 @@ function check_collision_new(my_pp,my_blocks){
                             $(my_block).removeClass('door-locked-top');
                         }
                         if(col_id == '1E'){
-                            $(my_block).removeClass('door-locked-left');
+                            $(my_block).removeClass('door-locked-right');
                         }
                         if(col_id == '1W'){
-                            $(my_block).removeClass('door-locked-right');
+                            $(my_block).removeClass('door-locked-left');
                         }
                         if(col_id == '1S'){
                             $(my_block).removeClass('door-locked-bottom');
@@ -260,27 +263,47 @@ function check_collision_new(my_pp,my_blocks){
 
                 return false;
 
+            }else if($(my_block).hasClass('stairs')){
+
+                console.log('STAIRS: '+ my_block.id);
+                
+
+                $('.floor').fadeOut('fast');
+                load_room(maps[loaded_map.portal.room]);
+
+                // get position of the stairs(portal) in this room...
+                stairs_top = loaded_map.portal.top;
+                stairs_left = loaded_map.portal.left;
+
+                $('.player').css('top',stairs_top);
+                $('.player').css('left',stairs_left);
+                $('.floor').fadeIn();
+
+                return false;
+
             }else if( $(my_block).hasClass('item') ){
+                // ITEM
                 // create pickup item function
                 // add item to inventory
                 // remove item from display or hide it
 
                 // what kind of item?
                 if (  $(my_block).hasClass('key') ){
+                    // KEY
+                    console.log('ITEM: KEY:'+ col_id);
                     inventory.items['key'] = true;
                     $(my_block).hide();
                     $('#inventory').append('<div class="inv-item"><img src="./items/key.png" /></div>');
                     loaded_map.items.key = false;
-                    console.log('ITEM:'+ col_id);
+                    
                     return false;
                 }
                 
                 
-            }
+            }// end check non block(doors and items)
         }else{
             // console.log( 'NO COLLISION!' );
-        }
-
+        } // end collision check
     });
     return my_col;
 }
@@ -288,6 +311,7 @@ function check_collision_new(my_pp,my_blocks){
 
 function get_next_image(dir){
 
+    // decide what the next animation image should be and chance it out.
     var cur_img = $('.player').css('background-image');
 
     var filename = cur_img.match(/.*\/(.*)$/)[1];
